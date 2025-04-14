7c0a7b3b4585c0ca728633d8ef62f7ae1cf7cdf9
Haotian Hang <hanghaotian@gmail.com>
2025-04-13 18:24:53 -0700

    update formatting (no any usage)

--- Source/main.cpp
+++ Source/main.cpp
@@ -9,40 +9,39 @@
 
 using namespace amrex;
 
-int main(int argc, char* argv[])
-{
-    amrex::Initialize(argc,argv);
+int main(int argc, char *argv[]) {
+  amrex::Initialize(argc, argv);
 
-    {
-        // timer for profiling
-        BL_PROFILE("main()");
+  {
+    // timer for profiling
+    BL_PROFILE("main()");
 
-        // wallclock time
-        //const auto strt_total = amrex::second();
+    // wallclock time
+    // const auto strt_total = amrex::second();
 
-        // constructor - reads in parameters from inputs file
-        //             - sizes multilevel arrays and data structures
-        AmrCoreLBM amr_core_lbm;
+    // constructor - reads in parameters from inputs file
+    //             - sizes multilevel arrays and data structures
+    AmrCoreLBM amr_core_lbm;
 
-        // initialize AMR data
-        amr_core_lbm.InitData();
+    // initialize AMR data
+    amr_core_lbm.InitData();
 
+    // advance solution to final time
 
-        // advance solution to final time
+    amr_core_lbm.Evolve();
 
-        amr_core_lbm.Evolve();
+    /*
+            // wallclock time
+            auto end_total = amrex::second() - strt_total;
 
-/*
-        // wallclock time
-        auto end_total = amrex::second() - strt_total;
+            if (amr_core_adv.Verbose()) {
+                // print wallclock time
+                ParallelDescriptor::ReduceRealMax(end_total
+       ,ParallelDescriptor::IOProcessorNumber()); amrex::Print() << "\nTotal
+       Time: " << end_total << '\n';
+            }
+    */
+  }
 
-        if (amr_core_adv.Verbose()) {
-            // print wallclock time
-            ParallelDescriptor::ReduceRealMax(end_total ,ParallelDescriptor::IOProcessorNumber());
-            amrex::Print() << "\nTotal Time: " << end_total << '\n';
-        }
-*/        
-    }
-
-    amrex::Finalize();
+  amrex::Finalize();
 }
